c1c9d18c4a3bdef1870c36419a5959144de995de
KorotkayaNastuha <skiteikina11@gmail.com>
2022-10-26 23:21:17 +0300

    02-timer

---
+++ src/js/02-timer.js
@@ -0,0 +1,28 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.5.min.css";
+
+const input = document.querySelector('#datetime-picker');
+const btnStart = document.querySelector('[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+btnStart.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      if (selectedDates[0] < new Date()) {
+          Notiflix.Notify.failure("Please choose a date in the future");
+        btnStart.disabled = false;  
+      };
+      
+  },
+};
